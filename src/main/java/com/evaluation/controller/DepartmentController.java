f370600e04118b954b706168527125c228fa9445
yorez <yorez@naver.com>
2019-05-16 11:31:56 +0900

    Complete Department Controller CRUD by using Modal

--- src/main/java/com/evaluation/controller/DepartmentController.java
+++ src/main/java/com/evaluation/controller/DepartmentController.java
@@ -1,7 +1,5 @@
 package com.evaluation.controller;
 
-import java.util.List;
-
 import com.evaluation.domain.Department;
 import com.evaluation.service.DepartmentService;
 import com.evaluation.service.TurnService;
@@ -13,7 +11,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -28,9 +28,62 @@ public class DepartmentController {
     @Autowired
     TurnService turnService;
 
+    @PostMapping("/register")
+    public String register(long tno, Department department, RedirectAttributes rttr) {
+        log.info("department register by " + tno + department);
+
+        rttr.addFlashAttribute("msg", "register");
+        rttr.addAttribute("tno", tno);
+
+        long cno = turnService.get(tno).get().getCompany().getCno();
+        department.setCno(cno);
+
+        departmentService.register(department);
+
+        return "redirect:/department/list";
+    }
+
+    @PostMapping("/modify")
+    public String modify(Department department, long tno, PageVO vo, RedirectAttributes rttr) {
+        log.info("modify " + department);
+
+        departmentService.read(department.getDno()).ifPresent(origin -> {
+            origin.setDepartment1(department.getDepartment1());
+            origin.setDepartment2(department.getDepartment2());
+            origin.setUpdateId(department.getUpdateId());
+
+            departmentService.modify(origin);
+            rttr.addFlashAttribute("msg", "modify");
+        });
+
+        rttr.addAttribute("tno", tno);
+        rttr.addAttribute("page", vo.getPage());
+        rttr.addAttribute("size", vo.getSize());
+        rttr.addAttribute("type", vo.getType());
+        rttr.addAttribute("keyword", vo.getKeyword());
+
+        return "redirect:/department/list";
+    }
+
+    @PostMapping("/remove")
+    public String remove(long dno, long tno, PageVO vo, RedirectAttributes rttr) {
+        log.info("remove " + dno);
+
+        departmentService.remove(dno);
+
+        rttr.addFlashAttribute("msg", "remove");
+        rttr.addAttribute("tno", tno);
+        rttr.addAttribute("page", vo.getPage());
+        rttr.addAttribute("size", vo.getSize());
+        rttr.addAttribute("type", vo.getType());
+        rttr.addAttribute("keyword", vo.getKeyword());
+
+        return "redirect:/department/list";
+    }
+
     @GetMapping("/list")
     public void readList(long tno, PageVO vo, Model model) {
-        log.info("controller : department list by " + tno);
+        log.info("department list by " + tno);
 
         model.addAttribute("tno", tno);
 
