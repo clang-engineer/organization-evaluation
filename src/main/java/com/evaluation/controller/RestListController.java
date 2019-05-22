f096d4ad2f23cddce1fbf58e84d56d5667f46081
yorez <yorez@naver.com>
2019-05-22 18:40:17 +0900

    "Relation360" : RestController + Presentation layer for distinct evaluator list

--- src/main/java/com/evaluation/controller/RestListController.java
+++ src/main/java/com/evaluation/controller/RestListController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.evaluation.domain.Staff;
 import com.evaluation.service.StaffService;
+import com.evaluation.service.TurnService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,11 +22,23 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RestListController {
     private StaffService staffService;
+    private TurnService turnService;
 
-    @GetMapping("/staff/list/{cno}/{tno}")
-    public ResponseEntity<List<Staff>> getStaffListEntity(@PathVariable("cno") long cno,
-            @PathVariable("tno") long tno) {
-        log.info("getList....");
-        return new ResponseEntity<>(staffService.getAllListExcludeEvalated(cno, tno), HttpStatus.OK);
+    @GetMapping("/staff/evaluated/{tno}")
+    public ResponseEntity<List<Staff>> getStaffForEvaluated(@PathVariable("tno") long tno) {
+        log.info("get All Staff List Exclude Evaluated....");
+
+        long cno = turnService.get(tno).get().getCompany().getCno();
+        return new ResponseEntity<>(staffService.getEvaluatedList(cno, tno), HttpStatus.OK);
     }
+
+    @GetMapping("/staff/evaluator/{tno}/{sno}")
+    public ResponseEntity<List<Staff>> getStaffForEvaluator(@PathVariable("tno") long tno,
+            @PathVariable("sno") long sno) {
+        log.info("get All Staff List....");
+
+        long cno = turnService.get(tno).get().getCompany().getCno();
+        return new ResponseEntity<>(staffService.getEvaluatorList(cno, tno, sno), HttpStatus.OK);
+    }
+
 }
