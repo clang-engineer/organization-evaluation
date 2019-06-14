e5b7c2fd9584348316e7a0feeb4a09f07fcf393e
yorez <yorez@naver.com>
2019-06-14 12:25:20 +0900

    "Department" : leader CRU
    - 삭제 관련 이슈가 남음.. select에서 공란 전달 했을 때 어떻게 해야하는지 ㅜㅜ!!

--- src/main/java/com/evaluation/controller/DepartmentController.java
+++ src/main/java/com/evaluation/controller/DepartmentController.java
@@ -1,12 +1,17 @@
 package com.evaluation.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.evaluation.domain.Department;
+import com.evaluation.domain.Leader;
+import com.evaluation.domain.Staff;
 import com.evaluation.service.DepartmentService;
+import com.evaluation.service.StaffService;
 import com.evaluation.service.TurnService;
 import com.evaluation.vo.PageMaker;
 import com.evaluation.vo.PageVO;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,18 +20,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/department/*")
 @Slf4j
+@AllArgsConstructor
 public class DepartmentController {
 
-    @Autowired
     DepartmentService departmentService;
 
-    @Autowired
     TurnService turnService;
+
+    StaffService staffService;
 
     @PostMapping("/register")
     public String register(long tno, Department department, RedirectAttributes rttr) {
@@ -46,7 +53,6 @@ public class DepartmentController {
     @PostMapping("/modify")
     public String modify(Department department, long tno, PageVO vo, RedirectAttributes rttr) {
         log.info("modify " + department);
-
 
         departmentService.modify(department);
         rttr.addFlashAttribute("msg", "modify");
@@ -86,6 +92,57 @@ public class DepartmentController {
         Page<Department> result = departmentService.getListWithPaging(cno, vo);
         model.addAttribute("result", new PageMaker<>(result));
 
+        List<Staff> leader = new ArrayList<Staff>();
+        result.getContent().forEach(department -> {
+            if (department.getLeader() != null) {
+                staffService.read(department.getLeader().getSno()).ifPresent(staff -> {
+                    leader.add(staff);
+                });
+            }
+        });
+        model.addAttribute("leaderList", leader);
+
     }
 
+    @GetMapping("/leader")
+    public void readLeader(long dno, long tno, PageVO vo, Model model) {
+        departmentService.read(dno).ifPresent(department -> {
+            model.addAttribute("team", department.getLeader());
+
+            if (department.getLeader() != null) {
+                staffService.read(department.getLeader().getSno()).ifPresent(staff -> {
+                    model.addAttribute("leader", staff);
+                });
+            }
+        });
+
+        long cno = turnService.get(tno).get().getCno();
+        staffService.readBycno(cno).ifPresent(origin -> {
+            model.addAttribute("staffList", origin);
+        });
+
+        model.addAttribute("dno", dno);
+        model.addAttribute("tno", tno);
+        model.addAttribute("page", vo.getPage());
+        model.addAttribute("size", vo.getSize());
+        model.addAttribute("type", vo.getType());
+        model.addAttribute("keyword", vo.getKeyword());
+    }
+
+    @PostMapping("/leader/register")
+    public String registerLeader(Leader leader, long dno, long tno, PageVO vo, RedirectAttributes rttr) {
+
+        departmentService.read(dno).ifPresent(origin -> {
+            origin.setLeader(leader);
+            departmentService.modify(origin);
+        });
+
+        rttr.addAttribute("tno", tno);
+        rttr.addAttribute("page", vo.getPage());
+        rttr.addAttribute("size", vo.getSize());
+        rttr.addAttribute("type", vo.getType());
+        rttr.addAttribute("keyword", vo.getKeyword());
+
+        return "redirect:/department/list";
+    }
 }
