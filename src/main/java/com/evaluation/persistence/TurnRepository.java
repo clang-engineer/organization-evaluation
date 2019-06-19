a71da49ff3f0fd88238edbbc7ecdf9cdb81cd505
yorez <yorez@naver.com>
2019-06-19 21:22:57 +0900

    "Turn" : modify SQL that get turns which have status doesn't like inactivation or setting
    - infoMbo에 상태단계 추가하고, login시 turn가져오는 쿼리 수정.

--- src/main/java/com/evaluation/persistence/TurnRepository.java
+++ src/main/java/com/evaluation/persistence/TurnRepository.java
@@ -9,21 +9,22 @@ import com.evaluation.domain.Turn;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface TurnRepository extends CrudRepository<Turn, Long> {
 
-	//admin에서 회사별로 turn조회할 때, rest controller에서 사용
+	// admin에서 회사별로 turn조회할 때, rest controller에서 사용
 	@Query("SELECT t FROM Turn t WHERE t.cno=?1 AND t.tno>0 ORDER BY t.tno ASC")
 	public Optional<List<Turn>> getTurnsOfCompany(Long cno);
 
 	// 진행 중인 설문 정보 불러오기 한 회사 안에서 상태와 시작, 종료날짜를 제약 조건으로 한다.
-	@Query("SELECT t FROM Turn t WHERE t.cno=?1 AND t.tno>0 AND (t.info360.status='survey' OR t.info360.status='count') AND t.info360.startDate<=?2 AND t.info360.endDate>=?2 ORDER BY t.tno DESC")
-	public Optional<List<Turn>> getTurnsInSurvey(Long cno, LocalDateTime threshold);
+	@Query("SELECT t FROM Turn t WHERE t.cno=:cno AND t.tno>0 AND t.info360.status NOT IN ('setting', 'inactivation')  AND :time BETWEEN t.info360.startDate AND t.info360.endDate ORDER BY t.tno DESC")
+	public Optional<List<Turn>> getTurnsInSurvey(@Param("cno") Long cno, @Param("time") LocalDateTime threshold);
 
 	// 진행 중인 설문 정보 불러오기 한 회사 안에서 상태와 시작, 종료날짜를 제약 조건으로 한다.
-	@Query("SELECT t FROM Turn t WHERE t.cno=?1 AND t.tno>0 AND (t.infoMBO.status='survey' OR t.infoMBO.status='count') AND t.infoMBO.startDate<=?2 AND t.infoMBO.endDate>=?2 ORDER BY t.tno DESC")
-	public Optional<List<Turn>> getTurnsInMBO(Long cno, LocalDateTime threshold);
+	@Query("SELECT t FROM Turn t WHERE t.cno=:cno AND t.tno>0 AND t.infoMBO.status NOT IN ('setting', 'inactivation') AND :time BETWEEN t.infoMBO.startDate AND t.infoMBO.endDate ORDER BY t.tno DESC")
+	public Optional<List<Turn>> getTurnsInMBO(@Param("cno") Long cno, @Param("time") LocalDateTime threshold);
 
 	@Modifying
 	@Transactional
